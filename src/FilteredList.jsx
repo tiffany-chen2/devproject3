e98c59654b24483f59d1ced1c8e32c18e19e3654
tiffany-chen2 <71267601+tiffany-chen2@users.noreply.github.com>
2020-12-04 05:53:14 +0800

    commit2

--- src/FilteredList.jsx
+++ src/FilteredList.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Navbar, Nav } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import DisplayList from "./DisplayList.jsx"
 
